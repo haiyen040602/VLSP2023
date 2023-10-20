16c79865364ac91935cc847d7f7d1261bd500d7a
haiyen040602 <20021480@vnu.edu.vn>
2023-10-21 02:08:37 +0700

    commit

--- coqe_base/model_utils/layer_utils.py
+++ coqe_base/model_utils/layer_utils.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-# from torchcrf import CRF
-from TorchCRF import CRF
+from torchcrf import CRF
+# from TorchCRF import CRF
 
 from transformers import BertModel, BertLayer
 
